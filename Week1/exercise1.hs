3d4d630dfc6e839b3326b1b4bf159df01c562bf1
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-19 14:30:06 +0900

    Add exercise instruction
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week1/exercise1.hs
+++ Week1/exercise1.hs
@@ -1,3 +1,21 @@
+{-
+Exercise 1 
+
+We need to first find the digits of a number. Define the functions
+
+toDigits :: Integer -> [Integer]
+toDigitsRev :: Integer -> [Integer]
+
+toDigits should convert positive Integers to a list of digits. 
+(For 0 or negative inputs, toDigits should return the empty list.)
+toDigitsRev should do the same, but with the digits reversed.
+
+Example: toDigits 1234 == [1,2,3,4]
+Example: toDigitsRev 1234 == [4,3,2,1]
+Example: toDigits 0 == []
+Example: toDigits (-17) == []
+-}
+
 toDigits :: Integer -> [Integer]
 toDigits input
   | input <= 0 = []
