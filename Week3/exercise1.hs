63b84e7412983138bd6b4e49ad91cc4163a616c3
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-23 22:35:33 +0900

    Initial commit for Week3
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ Week3/exercise1.hs
@@ -0,0 +1,23 @@
+{-  
+Exercise 1 
+
+Hopscotch
+
+Your first task is to write a function
+
+skips :: [a] -> [[a]]
+
+The output of skips is a list of lists. The first list in the output should
+be the same as the input list. The second list in the output should
+contain every second element from the input list. . . and the nth list in
+the output should contain every nth element from the input list.
+For example:
+
+skips "ABCD" == ["ABCD", "BD", "C", "D"]
+skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
+skips [1] == [[1]]
+skips [True,False] == [[True,False], [False]]
+skips [] == []
+
+Note that the output should be the same length as the input.
+-}
