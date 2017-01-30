6020104778ec1a19baad623be2d7a5d6d1e9e131
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-30 12:11:55 +0900

    Initial commit for week4
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ Week4/exercise3.hs
@@ -0,0 +1,39 @@
+{-
+Exercise 3: More folds!
+
+1. Implement a function
+
+xor :: [Bool] -> Bool
+
+which returns True if and only if there are an odd number of True
+values contained in the input list. It does not matter how many
+False values the input list contains. For example,
+
+xor [False, True, False] == True
+xor [False, True, False, False, True] == False
+
+Your solution must be implemented using a fold.
+
+2. Implement map as a fold. That is, complete the definition
+map’ :: (a -> b) -> [a] -> [b]
+map’ f = foldr ...
+in such a way that map’ behaves identically to the standard map
+function.
+
+3. (Optional) Implement foldl using foldr. That is, complete the
+definition
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr ...
+in such a way that myFoldl behaves identically to the standard
+foldl function.
+
+Hint: Study how the application of foldr and foldl work out:
+foldr f z [x1, x2, ..., xn] == x1 ‘f‘ (x2 ‘f‘ ... (xn ‘f‘ z)...)
+foldl f z [x1, x2, ..., xn] == (...((z ‘f‘ x1) ‘f‘ x2) ‘f‘...) ‘f‘ xn-}
+
+xor :: [Bool] -> Bool
+
+map1 :: (a -> b) -> [a] -> [b]
+
+myfoldl :: (a -> b -> a) -> a -> [b] -> a
---
+++ Week4/exercise4.hs
@@ -0,0 +1,23 @@
+{-
+Exercise 4: Finding primes
+
+Read about the Sieve of Sundaram. Implement the algorithm using
+function composition. Given an integer n, your function should
+generate all the odd prime numbers up to 2n + 2.
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram = ...
+
+To give you some help, below is a function to compute the Cartesian
+product of two lists. This is similar to zip, but it produces all
+possible pairs instead of matching up the list elements. For example,
+
+cartProd [1,2] [’a’,’b’] == [(1,’a’),(1,’b’),(2,’a’),(2,’b’)]
+
+It’s written using a list comprehension, which we haven’t talked about
+in class (but feel free to research them).
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]-}
+
+sieveSundaram :: Integer -> [Integer]
