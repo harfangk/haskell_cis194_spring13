44a4d178246e34f81b9f0a9d181582cd704afb9e
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-30 22:03:32 +0900

    Solve exercise 3
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week4/exercise3.hs
+++ Week4/exercise3.hs
@@ -33,7 +33,10 @@ foldr f z [x1, x2, ..., xn] == x1 ‘f‘ (x2 ‘f‘ ... (xn ‘f‘ z)...)
 foldl f z [x1, x2, ..., xn] == (...((z ‘f‘ x1) ‘f‘ x2) ‘f‘...) ‘f‘ xn-}
 
 xor :: [Bool] -> Bool
+xor = foldl (\acc _ -> not acc) False . filter (==True)
 
-map1 :: (a -> b) -> [a] -> [b]
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
 
 myfoldl :: (a -> b -> a) -> a -> [b] -> a
+myfoldl f base xs = foldr (flip f) base xs
