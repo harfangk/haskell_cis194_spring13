eb1010a1898f6f08b844b83055eb296de60454dd
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-31 04:40:44 +0900

    Solve exercise4
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week5/exercise4.hs
+++ Week5/exercise4.hs
@@ -48,21 +48,52 @@ working. It’s great how easy it is for us to swap in new semantics for
 the same syntactic expression!
 -}
 
-data ExprT = Lit Integer
-           | Add ExprT ExprT
-           | Mul ExprT ExprT
-           deriving (Show, Eq)
+import ExprT
+import Parser
 
 eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add x y) = (eval x) + (eval y)
+eval (Mul x y) = (eval x) * (eval y)
 
 evalStr :: String -> Maybe Integer
+evalStr x =
+  case parsedExp of
+    Just exp -> Just (eval exp)
+    _ -> Nothing
+  where parsedExp = parseExp Lit Add Mul x
 
 class Expr a where
-  lit :: a -> Integer 
-  add :: a -> a -> Integer
-  mul :: a -> a -> Integer
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
 
 instance Expr ExprT where
-  lit x =
-  add x y =
-  mul x y = 
+  lit x = Lit x
+  add x y = Add x y
+  mul x y = Mul x y
+
+instance Expr Integer where
+  lit x = x
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit x  
+    | x > 0 = True
+    | otherwise = False
+  add x y = x || y
+  mul x y = x && y
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
