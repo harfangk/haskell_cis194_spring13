b3f6296905f658664ac5093fbe44ff7a2cd3fbd8
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-10 20:32:26 +0900

    Complete exercise1
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week6/exercise1.hs
+++ Week6/exercise1.hs
@@ -47,5 +47,9 @@ each element of the list fibs1 to the next. Surely we can do better.
 -}
 
 fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
+fibs1 = map fib [0..]
