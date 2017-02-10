b310ab212e4c9433dd19117bd14534fb09b6554d
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-11 00:56:18 +0900

    Complete exercise2
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week6/exercise2.hs
+++ Week6/exercise2.hs
@@ -14,7 +14,15 @@ use standard recursion pattern(s) from the Prelude as appropriate.
 -}
 
 fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
+fibs1 = map fib [0..]
 
 fibs2 :: [Integer]
+fibs2 = fibs2_helper 0 1
+
+fibs2_helper :: Integer -> Integer -> [Integer]
+fibs2_helper a b = a : fibs2_helper b (a+b)
