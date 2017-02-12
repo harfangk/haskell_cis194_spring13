2e41aec3e61442be9e2353fcd9971e744c6d9fb6
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-13 01:45:56 +0900

    Solve exercise6
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week6/exercise6.hs
+++ Week6/exercise6.hs
@@ -85,17 +85,60 @@ fibs3 :: Stream Integer
 -}
 
 fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
+fibs1 = map fib [0..]
 
 fibs2 :: [Integer]
+fibs2 = fibs2_helper 0 1
+
+fibs2_helper :: Integer -> Integer -> [Integer]
+fibs2_helper a b = a : fibs2_helper b (a+b)
+
+data Stream a = Cons a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
 
 streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
 
 streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
 nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
 ruler :: Stream Integer
+ruler = ruler_helper 0
+
+ruler_helper :: Integer -> Stream Integer
+ruler_helper x = interleaveStreams (streamRepeat x) (ruler_helper (x+1))
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
+
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger n = Cons n (streamRepeat 0)
+  negate x = streamMap negate x
+  (+) (Cons x xs) (Cons y ys) = Cons (x + y) (xs + ys)
+  (*) (Cons x xs) (Cons y ys) = Cons (x * y) (streamMap (*x) ys + xs * (Cons y ys))
+  
+instance Fractional (Stream Integer) where
+  (/) (Cons x xs) (Cons y ys) = q 
+    where q = Cons (div x y) (streamMap (`div` y) (xs - (q * ys)))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x ^ (2 :: Integer))
