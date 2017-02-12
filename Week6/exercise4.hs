4dd3f43406b34ee7663f4b6059eb14259b89940e
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-12 17:15:14 +0900

    Solve exercise4
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- Week6/exercise4.hs
+++ Week6/exercise4.hs
@@ -51,7 +51,10 @@ streamToList :: Stream a -> [a]
 streamToList (Cons x xs) = x : streamToList xs
 
 streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
