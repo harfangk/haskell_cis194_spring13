5dae18102281b9c200cd2c17cadf31c30ffb01db
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-03-27 17:23:48 +0900

    Complete exercise 1
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ Week8/Party.hs
@@ -0,0 +1,15 @@
+module Party where
+
+import Employee
+
+glCons :: Employee -> GuestList -> GuestList
+glCons emp@(Emp _ empFun) (GL empList glFun) = GL (emp : empList) (glFun + empFun)
+
+instance Monoid GuestList where
+  mappend (GL empList1 fun1) (GL empList2 fun2) = GL (empList1 ++ empList2) (fun1 + fun2)
+  mempty = GL [] 0
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1@(GL empList1 fun1) gl2@(GL empList2 fun2) 
+  | fun1 >= fun2 = gl1
+  | otherwise = gl2
